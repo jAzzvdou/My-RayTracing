e63aca236ca9e187da4ada5280a6b448f002706f
jpedrobacker <jpedrobacker@outlook.com>
2025-03-06 19:30:56 -0300

    chapter 11 | test 5 to review

--- Screen/render.c
+++ Screen/render.c
@@ -3,7 +3,7 @@
 t_object	glass_sphere(void)
 {
 	t_object sp = new_object(SP);
-	sp.material.transparency = 0.3;
+	sp.material.transparency = 0;
 	sp.material.refractive_index = 1.5;
 	return (sp);
 }
@@ -16,19 +16,25 @@ void render_scene(t_minilibx *libx)
 	floor.material = material();
 	floor.material.color = color(1, 0.9, 0.9);
 	floor.material.spec = 0;
+	set_transform(&floor, translation(0, -1, 0));
+	floor.material.pattern = new_pattern(STRIPE, color(1, 0, 0), color(0, 1, 1));
+	set_pattern_transform(&floor.material.pattern, scaling(0.3, 0.3, 0.3));
 
 	// Criar esfera de vidro
-	t_object glass = glass_sphere();
-	glass.transformed = translation(0, 1, 0); // No centro
+	t_object glass = new_object(SP);
+	glass.material.transparency = 0.9;
+	glass.material.refractive_index = 1;
+	set_transform(&glass, translation(0, 1, 3));// No centro
 
 	// Criar esfera opaca atrás da esfera de vidro
 	t_object opaque_sphere = new_object(SP);
-	opaque_sphere.transformed = translation(0, 1, 3);
+	//opaque_sphere.transformed = translation(0, 1, 3);
 	opaque_sphere.material.color = color(1, 0, 0); // Vermelha
+	set_transform(&opaque_sphere, scaling(0.5, 0.5, 0.5));
 
 	// Criar mundo e câmera
 	t_world w = world();
-	t_camera cam = camera(400, 200, M_PI / 3);
+	t_camera cam = camera(HEIGHT, WIDTH, M_PI / 3);
 	cam.transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
 
 	add_object(&w.object, opaque_sphere);
@@ -103,30 +109,24 @@ t_intersection *inter_index(t_intersection *inter, int index)
 
 void render_tests(t_minilibx *libx)
 {
-	render_scene(libx);
+	//render_scene(libx);
 	(void)libx;
-	/*t_world w = default_world();
-
-	t_object a = new_object(PL);
-	a.material.amb = 1.0;
-	a.material.pattern = new_pattern(STRIPE, color(1, 1, 1), color(0, 0, 0));
-	add_object(&w.object, a);
+	t_world w = default_world();
 
 	t_object b = new_object(SP);
 	b.material.transparency = 1.0;
 	b.material.refractive_index = 1.5;
 	add_object(&w.object, b);
 
-	t_ray r = ray(point(0, 0, 0.1), vector(0, 1, 0));
+	t_ray r = ray(point(0, 0, -5), vector(0, 0, 1));
 	t_intersection *xs = NULL;
-	add_intersection(&xs, intersection(a, -0.9899));
-	add_intersection(&xs, intersection(b, -0.4899));
-	add_intersection(&xs, intersection(b, 0.4899));
-	add_intersection(&xs, intersection(a, 0.9899));
+	add_intersection(&xs, intersection(b, 4));
+	add_intersection(&xs, intersection(b, 6));
 
-	t_comps comps = prepare_computations(xs[2], r, xs);
+	t_comps comps = prepare_computations(*xs, r, xs);
 	t_color result = refracted_color(w, comps, 5);
 
-	printf("Expected: R -> 0.93642 | G -> 0.68642 | B -> 0.68642 \nResult: R -> %f| G -> %f| B -> %f\n", result.r, result.g, result.b);
-*/
+	printf("Expected: R -> 0.93642 | G -> 0.68642 | B -> 0.68642 \n");
+	printf("Result: R -> %f| G -> %f| B -> %f\n", result.r, result.g, result.b);
+
 }
