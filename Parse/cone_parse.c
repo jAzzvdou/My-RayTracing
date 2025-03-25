9e021114e59b5a4d13d1227747c176df6b728cc4
jazevedo <jazevedo@student.42.rio>
2025-03-25 01:25:38 -0300

    feat: parsing texture at cone, cylinder and plane

--- Parse/cone_parse.c
+++ Parse/cone_parse.c
@@ -6,11 +6,57 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 17:35:17 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/03/24 11:53:42 by jazevedo         ###   ########.fr       */
+/*   Updated: 2025/03/25 01:21:42 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/minirt.h"
+
+int     get_pattern_cone(char **splited, t_pattern *pattern)
+{
+        t_color a;
+        t_color b;
+
+        if (!my_strcmp(splited[9], "null") && !my_strcmp(splited[12], "null"))
+                return (1);
+        if (my_strcmp(splited[9], "null") && my_strcmp(splited[12], "null"))
+                return (0);
+        if (my_strcmp(splited[12], "null") && valid_path(splited[12]))
+        {
+                *pattern = new_pattern(TEXTURE, color(0, 0, 0), color(0, 0, 0));
+                pattern->texture_path = my_strjoin("10Patterns/Textures/", splited[12]);
+        }
+        else if (!get_color(splited[10], &a) || !get_color(splited[11], &b))
+                return (0);
+        else if (!my_strcmp(splited[9], "stripe"))
+                *pattern = new_pattern(STRIPE, a, b);
+        else if (!my_strcmp(splited[9], "gradient"))
+                *pattern = new_pattern(GRADIENT, a, b);
+        else if (!my_strcmp(splited[9], "ring"))
+                *pattern = new_pattern(RING, a, b);
+        else if (!my_strcmp(splited[9], "checker"))
+                *pattern = new_pattern(CHECKER, a, b);
+        else
+                return (0);
+        return (1);
+}
+
+int     valid_cone(char **splited, t_point *p, t_vector *n, t_object *n_cy)
+{
+        if (!valid_line_count(splited, 13)
+                || !get_coords(splited[1], p)
+                || !get_dir(splited[2], n)
+                || !get_double(splited[3], &n_cy->radius)
+                || !get_double(splited[4], &n_cy->maximum)
+                || !get_color(splited[5], &n_cy->material.color)
+                || !get_transparency(splited[6], &n_cy->material.transparency)
+                || !get_reflective(splited[7], splited[8],
+                                &n_cy->material.reflective,
+                                &n_cy->material.refractive_index)
+                || !get_pattern_cone(splited, &n_cy->material.pattern))
+                return (0);
+        return (1);
+}
 
 int	cone_parse(t_world *w, char *line)
 {
@@ -21,12 +67,7 @@ int	cone_parse(t_world *w, char *line)
 
 	n_cn = new_object(CN);
 	splited = my_split(line, ' ');
-	if (!valid_line_count(splited, 9)
-		|| !get_coords(splited[1], &p)
-		|| !get_dir(splited[2], &n)
-		|| !get_double(splited[3], &n_cn.radius)
-		|| !get_double(splited[4], &n_cn.maximum)
-		|| !get_color(splited[5], &n_cn.material.color))
+	if (!valid_cone(splited, &p, &n, &n_cn))
 	{
 		splited = memcard(splited, VECTOR, FREE, 0);
 		return (err(RED, "Error! cone_parse ko\n", RESET), 0);
--- Parse/plane_parse.c
+++ Parse/plane_parse.c
@@ -6,11 +6,55 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 16:46:35 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/03/24 11:54:01 by jazevedo         ###   ########.fr       */
+/*   Updated: 2025/03/25 00:47:45 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/minirt.h"
+
+int     get_pattern_plane(char **splited, t_pattern *pattern)
+{
+        t_color a;
+        t_color b;
+
+        if (!my_strcmp(splited[7], "null") && !my_strcmp(splited[10], "null"))
+                return (1);
+        if (my_strcmp(splited[7], "null") && my_strcmp(splited[10], "null"))
+                return (0);
+        if (my_strcmp(splited[10], "null") && valid_path(splited[10]))
+        {
+                *pattern = new_pattern(TEXTURE, color(0, 0, 0), color(0, 0, 0));
+                pattern->texture_path = my_strjoin("10Patterns/Textures/", splited[10]);
+        }
+        else if (!get_color(splited[8], &a) || !get_color(splited[9], &b))
+                return (0);
+        else if (!my_strcmp(splited[7], "stripe"))
+                *pattern = new_pattern(STRIPE, a, b);
+        else if (!my_strcmp(splited[7], "gradient"))
+                *pattern = new_pattern(GRADIENT, a, b);
+        else if (!my_strcmp(splited[7], "ring"))
+                *pattern = new_pattern(RING, a, b);
+        else if (!my_strcmp(splited[7], "checker"))
+                *pattern = new_pattern(CHECKER, a, b);
+        else
+                return (0);
+        return (1);
+}
+
+int	valid_plane(char **splited, t_point *p, t_vector *n, t_object *n_pl)
+{
+	if (!valid_line_count(splited, 11)
+		|| !get_coords(splited[1], p)
+		|| !get_dir(splited[2], n)
+		|| !get_color(splited[3], &n_pl->material.color)
+                || !get_transparency(splited[4], &n_pl->material.transparency)
+                || !get_reflective(splited[5], splited[6],
+                        &n_pl->material.reflective,
+                        &n_pl->material.refractive_index)
+                || !get_pattern_plane(splited, &n_pl->material.pattern))
+		return (0);
+	return (1);
+}
 
 int	plane_parse(t_world *w, char *line)
 {
@@ -20,12 +64,8 @@ int	plane_parse(t_world *w, char *line)
 	t_object	n_pl;
 
 	n_pl = new_object(PL);
-	//n_pl.material = material();
 	splited = my_split(line, ' ');
-	if (!valid_line_count(splited, 9)
-		|| !get_coords(splited[1], &p)
-		|| !get_dir(splited[2], &n)
-		|| !get_color(splited[3], &n_pl.material.color))
+	if (!valid_plane(splited, &p, &n, &n_pl))
 	{
 		splited = memcard(splited, VECTOR, FREE, 0);
 		return (err(RED, "Error! plane_parse ko\n", RESET), 0);
